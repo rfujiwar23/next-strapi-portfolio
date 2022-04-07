de3a1fce7daf3e86b77bcb5e11603c2a47456a0b
Ryo Fujiwara <jjcproject@icloud.com>
2022-04-07 23:44:24 +0900

    add strapi tomorrow

--- pages/portfolio/[slug].jsx
+++ pages/portfolio/[slug].jsx
@@ -1,8 +1,28 @@
+import Layout from "../../components/Layout";
+
 const PortfolioItem = () => {
     return (
-        <div>
-            Testing Component
-        </div>
+        <Layout>
+            <div className="row">
+                <div className="portfolio-image text-center mb-4">
+                    <div className="col-md-12">
+                        <img src="https://via.placeholder.com/1000x500"/>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="portfolio-content">
+                    <div className="col-md-12">
+                        <div className="portfolio-headline text-center m-2">
+                            <h2>Project</h2>
+                        </div>
+                        <p>
+                            This is Mine, yes.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     );
 };
 
